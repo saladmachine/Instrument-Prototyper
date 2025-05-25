5efc382c1044299e11b58b8606b2acf9cecfd9c0
saladmachine <saladmachine123@gmail.com>
2025-05-25 18:51:47 -0400

    Update conf.py
    Copilot seems at the moment to be an useless piece of shit or rather a normal microsoft product.

--- docs/conf.py
+++ docs/conf.py
@@ -1,12 +1,32 @@
+# Configuration file for the Sphinx documentation builder.
+
 import os
 import sys
-sys.path.insert(0, os.path.abspath("../examples"))
+sys.path.insert(0, os.path.abspath('..'))  # Add project root to sys.path
 
-project = "saladmachine"
-author = "saladmachine"
-release = "0.1"
+project = 'Instrument-Prototyper'
+copyright = '2025, saladmachine'
+author = 'saladmachine'
+release = '0.1.0'
 
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon"]
-templates_path = ["_templates"]
-exclude_patterns = []
-html_theme = "alabaster"
+# Sphinx extensions
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',   # For Google/NumPy style docstrings
+    'sphinx.ext.viewcode',   # Adds links to highlighted source code
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# HTML output
+html_theme = 'alabaster'  # Or 'sphinx_rtd_theme' if you prefer
+
+# Napoleon settings (optional, but recommended)
+napoleon_google_docstring = True
+napoleon_numpy_docstring = True
+
+# Autodoc settings (optional)
+autoclass_content = 'both'     # Include both class docstring and __init__
+autodoc_member_order = 'bysource'
+autodoc_inherit_docstrings = True
